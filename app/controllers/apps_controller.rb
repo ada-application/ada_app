f19c0eae751c81cc6d500a969379e14b7e39e3c5
Renée De Voursney <renee@nird.us>
2013-08-07 14:39:00 -0700

    app cleanup

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,74 +1,45 @@
 class AppsController < ApplicationController
-  before_action :set_app, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+  load_resource :except => [:index, :create]
+  authorize_resource
 
-  # GET /apps
-  # GET /apps.json
   def index
     @apps = App.all
   end
 
-  # GET /apps/1
-  # GET /apps/1.json
   def show
   end
 
-  # GET /apps/new
-  def new
-    @app = App.new
-  end
-
-  # GET /apps/1/edit
   def edit
   end
 
-  # POST /apps
-  # POST /apps.json
   def create
     @app = App.new(app_params)
 
-    respond_to do |format|
-      if @app.save
-        format.html { redirect_to @app, notice: 'App was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @app }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @app.errors, status: :unprocessable_entity }
-      end
+    if @app.save
+      redirect_to @app, notice: 'Application was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /apps/1
-  # PATCH/PUT /apps/1.json
   def update
-    respond_to do |format|
-      if @app.update(app_params)
-        format.html { redirect_to @app, notice: 'App was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @app.errors, status: :unprocessable_entity }
-      end
+    if @app.update(app_params)
+      redirect_to @app, notice: 'Application was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /apps/1
-  # DELETE /apps/1.json
   def destroy
     @app.destroy
-    respond_to do |format|
-      format.html { redirect_to apps_url }
-      format.json { head :no_content }
-    end
+    redirect_to apps_url
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_app
-      @app = App.find(params[:id])
-    end
+private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def app_params
-      params.require(:app).permit(:accepted, :score, :reviewer_id, :rejected)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def app_params
+    params.require(:app).permit(:accepted, :score, :reviewer_id, :rejected)
+  end
 end
